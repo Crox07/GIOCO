3b4567df3caad3671d251774cde165591418edeb
Crox07 <ale.croce2007@gmail.com>
2024-05-05 11:38:57 +0200

    creazione di una finestra di gioco. solo schermo

--- schermo.py
+++ schermo.py
@@ -1,1 +1,14 @@
-print("ciao")
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((900, 700))
+screen.fill((255,255,255))
+pygame.display.set_caption("GIOCO")
+
+done = False
+while not done:
+    for ev in pygame.event.get():
+        if ev.type == QUIT:
+            done = True
+pygame.quit()
