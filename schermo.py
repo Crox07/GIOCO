8209daea269838271be468a022523a56437ad9f5
Crox07 <ale.croce2007@gmail.com>
2024-05-20 11:54:09 +0200

    prova di caricamento immagine

--- schermo.py
+++ schermo.py
@@ -5,10 +5,23 @@ pygame.init()
 screen = pygame.display.set_mode((900, 700))
 screen.fill((255,255,255))
 pygame.display.set_caption("GIOCO")
+PLAYER1=pygame.image.load("grass.png")
+
+
+
+
+#class player1:
+  #  def __init__(self, posx, posy, width = 100, height = 100) -> None:
+    #    self.image = pygame.image.load(PLAYER1)
+
 
 done = False
 while not done:
     for ev in pygame.event.get():
         if ev.type == QUIT:
             done = True
+    screen.blit(PLAYER1, (170,170))
+    pygame.display.update()
 pygame.quit()
+
+
