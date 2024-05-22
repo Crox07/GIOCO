32f1fda4f4c1e4e9c4be8ec2bcf3bef2d0419562
Crox07 <ale.croce2007@gmail.com>
2024-05-22 13:47:03 +0200

    Update schermo.py

--- schermo.py
+++ schermo.py
@@ -2,17 +2,17 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-screen = pygame.display.set_mode((900, 700))
-screen.fill((255,255,255))
+
+screen = pygame.display.set_mode((1100, 700))
+
 pygame.display.set_caption("GIOCO")
 
 space1=pygame.image.load('space1.jpg')
-space=pygame.image.load('space.png')
 
 
-
-
-
+titolo_su_schermo=True
+game_name=font.render("Space Wars", True, (240, 80, 255))
+name_rect=game_name.name_rect(midbottom=(550,350))
 #class player1:
   #  def __init__(self, posx, posy, width = 100, height = 100) -> None:
     #    self.image = pygame.image.load(PLAYER1)
@@ -23,10 +23,20 @@ while not done:
     for ev in pygame.event.get():
         if ev.type == QUIT:
             done = True
-    screen.blit(space,(-100,0))
+    
     screen.blit(space1,(0,0))
+    font = pygame.font.SysFont(None, 80)
+    
+    screen.blit(game_name,name_rect (midbottom))
+    
+    for ev in pygame.event.get():
+        if ev.type == pygame.KEYDOWN:
+            titolo_su_schermo=False
+
+    pygame.display.flip()
    
     pygame.display.update()
 pygame.quit()
+
 
 
